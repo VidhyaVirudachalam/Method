0e3b161453bf35b4e5e322ae07958fa4cd437ff7
vidhya <vidhyavyam@gmail.com>
2022-12-20 18:38:01 +0530

    message

---
+++ Method_Invoke.java
@@ -0,0 +1,35 @@
+package com.zoho.blufile;
+
+public class Method_Invoke {
+
+	public static void main(String[] args) {
+		
+		Method_Invoke info = new Method_Invoke();
+		
+		info.Candidate_Bloodgroup();
+		info.Candidate_Age();
+		info.Candidate_Class();
+		info.Candidate_Name();
+		info.Candidate_Score();
+	}
+	private void Candidate_Name() {
+		// TODO Auto-generated method stub
+         System.out.println("Candidate Name : Jungkook");
+	}
+	private void Candidate_Class() {
+		// TODO Auto-generated method stub
+        System.out.println("Candidate Class : 10th");
+	}
+	private void Candidate_Age() {
+		// TODO Auto-generated method stub
+        System.out.println("Candidate Age : 14");
+	}
+	private void Candidate_Score() {
+		// TODO Auto-generated method stub
+        System.out.println("Candidate Score : 480");
+	}
+	private void Candidate_Bloodgroup() {
+		// TODO Auto-generated method stub
+        System.out.println("Candidate Bloodgroup : B-ve");
+	}
+}
